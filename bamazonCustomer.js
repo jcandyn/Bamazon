3e852350791c3451beb7d3615fa2e56d4e4ed325
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-08-11 22:25:00 -0400

    got database to retrieve info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,35 @@
 var mysql = require("mysql");
+let inquirer = require("inquirer")
+let item_id_input;
+let item_quantity_input;
+let inventory;
+let updated_quantity;
+
+
+
+function inquirePrompt() {
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type: "input",
+        message: "What is the id of the item you would like to buy?",
+        name: "item_id_input"
+      },
+      {
+          type: "input",
+          message: "How many items with that id would you like to buy?",
+          name: "item_quantity_input" 
+      }
+  ])
+  .then(answers => {
+    console.log(answers)
+    item_id_input = answers.item_id_input
+    item_quantity_input = answers.item_quantity_input
+    checkInventory()
+    // Use user feedback for... whatever!!
+  });
+}
 
 
 
@@ -29,34 +60,46 @@ function afterConnection() {
     connection.query("SELECT * FROM products;", function(err, res) {
       if (err) throw err;
       console.table(res);
-      connection.end();
+      inquirePrompt()
     });
   }
 
+  function checkInventory() {
+    connection.query("SELECT stock_quantity FROM products WHERE item_id =? ;", [item_id_input], function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        inventory = res[0].stock_quantity
+        checkInventoryAvailability()
+      });
+  }
+
+  function checkInventoryAvailability() {
+      if (item_quantity_input > inventory) {
+          console.log("Insufficient quantity!")
+      }
+      else if ((item_quantity_input < inventory) || (item_quantity_input === inventory)) {
+          UpdateInventory()
+      }
+  }
+
+  function UpdateInventory() {
+      console.log(inventory)
+      console.log(item_quantity_input)
+      updated_quantity = inventory - item_quantity_input
+      console.log(updated_quantity)
+    connection.query("UPDATE products SET stock_quantity ? WHERE item_id ? ;", [updated_quantity], [item_id_input], function(err, res) {
+        if (err) throw err;
+        console.log("You have succesfully purchased this item!")
+        console.table(res);
+        showUpdatedTable()
+      });
+  }
 
 
-//   function whichArtist() {
-//     artist = process.argv.slice(2).join("")
-//     console.log(artist)
- 
-//     connection.query("SELECT song FROM top_playlist.songs WHERE artist = ?;",[artist],function(err, res) {
-//         console.log("-------------------------------------------------------")
-//       if (err) throw err;
-//       console.log(res);
-//       connection.end();
-//     });
-//   }
-
-//   function whichSong() {
-//     song = process.argv.slice(2).join("")
-//     console.log(song)
- 
-//     connection.query("SELECT * FROM top_playlist.songs WHERE song = ?;",[song],function(err, res) {
-//         console.log("-------------------------------------------------------")
-//       if (err) throw err;
-//       console.log(res);
-//       connection.end();
-//     });
-//   }
-
-
+function showUpdatedTable() { 
+        connection.query("SELECT * FROM products;", function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          connection.end();
+        });
+}
